8409acf955ce258384d123117a02f849832dbb60
mezealot24 <wiriya.on@gmail.com>
2024-07-15 00:54:22 +0700

    resume page update 75%

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -156,24 +156,78 @@ const Resume = () => {
 				opacity: 1,
 				transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
 			}}
-			className="min-h-[80vh] flex items-center justify-center0 py-12 xl:py-0"
+			className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
 		>
 			<div className="container mx-auto">
-				<Tabs
-					defaultValue="experience"
-					className="flex flex-col xl:flew-row gap-[60px]"
-				>
-					<TabsList className="flex flex-col w-full max-w-[380px] mx-auto lx:mx-0 gap-6">
-						<TabsTrigger value="experience">Experience</TabsTrigger>
-						<TabsTrigger value="education">Education</TabsTrigger>
-						<TabsTrigger value="skills">Skills</TabsTrigger>
-						<TabsTrigger value="about">About me</TabsTrigger>
-					</TabsList>
-					{/* content */}
-					<div className="min-h-[70vh] w-full">
-						<TabsContent value="experience" className="w-full">
-							experience
-						</TabsContent>
+				<Tabs defaultValue="experience" className="flex flex-col xl:flex-row">
+					<div className="flex flex-col xl:flex-row w-full gap-[60px]">
+						{/* TabsList (left side) */}
+						<div className="xl:w-1/4">
+							<TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+								<TabsTrigger value="experience">Experience</TabsTrigger>
+								<TabsTrigger value="education">Education</TabsTrigger>
+								<TabsTrigger value="skills">Skills</TabsTrigger>
+								<TabsTrigger value="about">About me</TabsTrigger>
+							</TabsList>
+						</div>
+
+						{/* TabsContent (right side) */}
+						<div className="xl:w-3/4 min-h-[70vh]">
+							<TabsContent value="experience" className="w-full">
+								<div className="flex flex-col gap-[30px] text-center xl:text-left">
+									<h3 className="text-4xl">{experience.title}</h3>
+									<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+										{experience.description}
+									</p>
+									<ScrollArea className="h-[400px]">
+										<ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+											{experience.items.map((item, index) => {
+												return (
+													<li
+														key={index}
+														className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+													>
+														<span className="text-accent">{item.duration}</span>
+														<p className="py-3 text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+															{item.position}
+														</p>
+														<div className="flex items-center gap-3">
+															{/* dot */}
+															<span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+															<p className="text-white/60">{item.company}</p>
+														</div>
+													</li>
+												);
+											})}
+										</ul>
+									</ScrollArea>
+								</div>
+							</TabsContent>
+							<TabsContent value="education" className="w-full">
+								<div className="flex flex-col gap-[30px] text-center xl:text-left">
+									<h3 className="text-4xl">{education.title}</h3>
+									<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+										{education.description}
+									</p>
+								</div>
+							</TabsContent>
+							<TabsContent value="skills" className="w-full">
+								<div className="flex flex-col gap-[30px] text-center xl:text-left">
+									<h3 className="text-4xl">{skills.title}</h3>
+									<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+										{skills.description}
+									</p>
+								</div>
+							</TabsContent>
+							<TabsContent value="about" className="w-full">
+								<div className="flex flex-col gap-[30px] text-center xl:text-left">
+									<h3 className="text-4xl">{about.title}</h3>
+									<p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+										{about.description}
+									</p>
+								</div>
+							</TabsContent>
+						</div>
 					</div>
 				</Tabs>
 			</div>
