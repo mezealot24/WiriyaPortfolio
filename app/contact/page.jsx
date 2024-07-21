cbdf61714180ebef7e3ce04c6aacb4bbecc8dba9
mezealot24 <wiriya.on@gmail.com>
2024-07-21 13:32:26 +0700

    Contact Page 50%

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -21,7 +21,7 @@ const info = [
 	{
 		icon: <FaPhoneAlt />,
 		title: "Phone",
-		description: "(+66) ",
+		description: "(+66) 92 328 7570 ",
 	},
 	{
 		icon: <FaEnvelope />,
@@ -49,9 +49,22 @@ const Contact = () => {
 				<div className="flex flex-col xl:flex-row gap-[30px]">
 					{/* form */}
 					<div className="xl:h-[54%] order-2 xl:order-none">
-						<form>form</form>
-						{/* info */}
+						<form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+							<h3 className="text4xl text-accent"> Let's work together </h3>
+							<p className="text-white/60">
+								Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+								Accusantium.
+							</p>
+							{/* input */}
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+								<Input type="firstname" placeholder="Firstname" />
+								<Input type="lastname" placeholder="Lastname" />
+								<Input type="email" placeholder="Email" />
+								<Input type="phone" placeholder="Phone number" />
+							</div>
+						</form>
 					</div>
+					{/* info */}
 					<div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
 						info
 					</div>
